e4d6ed24624e136669b8c62d60a5f7f0e1af10b1
Rhodri Pugh <rod@owsy.com>
2023-12-09 18:28:54 +0000

    asdf

--- init.lua
+++ init.lua
@@ -173,6 +173,14 @@ vim.keymap.set('n', '<leader>nn', function()
     require('phpactor').rpc('navigate', {})
 end)
 
+vim.keymap.set('n', '<leader>tt', function()
+    require('phpactor').rpc('transform', {})
+end)
+
+vim.keymap.set('n', '<leader>u', function()
+    require('phpactor').rpc('import_class', {})
+end)
+
 -- lsp configuration
 require'lspconfig'.phpactor.setup{
     on_attach = on_attach,
