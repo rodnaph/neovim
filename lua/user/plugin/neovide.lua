e8af2f0f7aba4536709f4cd5bf701f0a15bc6b9b
Rhodri Pugh <rod@owsy.com>
2023-12-23 07:54:56 +0000

    brain

--- lua/user/plugin/neovide.lua
+++ lua/user/plugin/neovide.lua
@@ -1,10 +1,11 @@
-vim.g.neovide_cursor_animation_length = 0
+if vim.g.neovide then
+    -- do not animate the cursor
+    vim.g.neovide_cursor_animation_length = 0
 
--- workaround neovide copy/paste
-vim.g.neovide_input_use_logo = 1
-vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
-vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
-
-
+    -- workaround neovide copy/paste
+    vim.g.neovide_input_use_logo = 1
+    vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+    vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+    vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+    vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+end
