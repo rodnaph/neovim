0546401042c92a2ae851bd7efc55b2d91dbaefd2
Rhodri Pugh <rod@owsy.com>
2024-02-07 16:58:18 +0000

    cmmmppppp

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,16 @@
+return {
+    'zbirenbaum/copilot.lua',
+    config = function ()
+        require('copilot').setup({
+            panel = {
+                auto_refresh = false,
+            },
+            suggestion = {
+                auto_trigger = true,
+                keymap = {
+                    accept = '<tab>',
+                },
+            },
+        })
+    end
+}
