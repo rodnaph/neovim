617ef5959c4e87a57b1e7d08ebb3997e50ad65e7
Rhodri Pugh <rod@owsy.com>
2024-05-23 07:48:32 +0100

    trbl

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/trouble.nvim",
+  branch = "dev",
+  keys = {
+    {
+      "<C-x>",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+  },
+}
