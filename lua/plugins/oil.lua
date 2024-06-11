239887a1a985809a00439a01a9661deeedd8424c
Rhodri Pugh <rod@owsy.com>
2024-06-11 22:48:15 +0100

    mix

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,11 +1,21 @@
 return {
-  'stevearc/oil.nvim',
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function ()
-      require('oil').setup({
-          keymaps = {},
-      })
+    'stevearc/oil.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function ()
+        require('oil').setup({
+            keymaps = {
+                ['<CR>'] = 'actions.select',
+                ['-'] = 'actions.parent',
+            },
+            use_default_keymaps = false,
+            view_options = {
+                is_always_hidden = function(name)
+                    return '..' == name
+                end,
+                show_hidden = true,
+            }
+        })
 
-      vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-  end
+        vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    end
 }
