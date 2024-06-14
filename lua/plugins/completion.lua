86f13e930e6f3ec17da7bb53e4d059b046830f4a
Rhodri Pugh <rod@owsy.com>
2024-06-14 08:21:10 +0100

    nocmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -3,8 +3,6 @@ return {
     dependencies = {
         'hrsh7th/cmp-nvim-lsp',
         'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-cmdline',
-        'hrsh7th/cmp-path',
     },
     config = function ()
         local cmp = require('cmp')
