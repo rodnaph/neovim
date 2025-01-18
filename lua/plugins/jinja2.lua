092a14c4529f30587619470bb151402e575df2c6
Rhodri Pugh <rod@owsy.com>
2025-01-18 17:00:34 +0000

    akjsdljaskldjalksdjlas

--- lua/plugins/jinja2.lua
+++ lua/plugins/jinja2.lua
@@ -3,5 +3,6 @@ return {
     config = function ()
         -- use jinja2 syntax for html.twig files
         vim.cmd('autocmd BufNewFile,BufRead *.html.twig set ft=jinja')
+        vim.cmd('autocmd BufNewFile,BufRead *.xml.twig set ft=jinja')
     end
 }
