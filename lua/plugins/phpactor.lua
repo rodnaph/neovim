3af893092133a0990fd7fbff3a4af6b19c6d30cb
Rhodri Pugh <rod@owsy.com>
2025-02-21 16:58:40 +0000

    navigggaaattteeee

--- lua/plugins/phpactor.lua
+++ lua/plugins/phpactor.lua
@@ -3,6 +3,8 @@ return {
     tag = 'v1.0.1',
     config = function ()
         local phpactor = require('phpactor')
+        local rpc = require('phpactor.rpc')
+        local utils = require('phpactor.utils')
 
         phpactor.setup({
             install = { bin = vim.fn.expand('$HOME/bin/phpactor') },
@@ -12,6 +14,20 @@ return {
         vim.keymap.set('n', '<leader>nn', function()
             phpactor.rpc('navigate', {})
         end, { desc = 'Navigate to a related file.' })
+
+        vim.keymap.set('n', '<leader>nu', function()
+            rpc.call('navigate', {
+                destination = 'unit_test',
+                source_path = utils.path()
+            })
+        end, { desc = 'Navigate to unit test.' })
+
+        vim.keymap.set('n', '<leader>ns', function()
+            rpc.call('navigate', {
+                destination = 'source',
+                source_path = utils.path()
+            })
+        end, { desc = 'Navigate to source.' })
 
         vim.keymap.set('n', '<leader>tt', function()
             phpactor.rpc('transform', {})
