f591b52ef31f06ed9c2f8740b7d27448f5a1a29c
Rhodri Pugh <rod@owsy.com>
2025-12-09 16:55:54 +0000

    upbl

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,7 +3,7 @@ return {
   dependencies = {
       'rafamadriz/friendly-snippets',
   },
-  version = 'v1.4.1',
+  version = 'v1.7.0',
   opts = {
     keymap = { 
         ['<C-y>'] = { 'fallback' },
